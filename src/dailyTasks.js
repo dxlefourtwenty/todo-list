c1d57341d13c9c2e0830393170c1b388e1b663a7
Dale Peligro <daledoesminecraft2013@gmail.com>
2025-10-19 21:28:20 -0700

    Scheduled, Daily layouts implemented

---
+++ src/dailyTasks.js
@@ -0,0 +1,18 @@
+const DailyTasks = (() => {
+
+    function newTask() {
+
+    }
+
+    function editTask() {
+
+    }
+
+    return {
+        newTask,
+        editTask
+    }
+
+})();
+
+export default DailyTasks;
